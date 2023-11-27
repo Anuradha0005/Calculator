5908d620fb6dabc338563522bcd83f5780133c89
Anuradha0005 <144481666+Anuradha0005@users.noreply.github.com>
2023-11-27 10:35:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+let inputBox = document.getElementById('inputBox')
+let buttons = document.querySelectorAll('button')
+
+let string = ''
+
+buttons.forEach(element =>{
+    element.addEventListener('click', (b)=>{
+        if(b.target.innerText == '='){
+            string = String(eval(string))
+            inputBox.value = string;
+        }
+
+        else if(b.target.innerText == 'AC'){
+            string = ''
+            inputBox.value = string;
+        }
+
+        else if(b.target.innerText == 'DEL'){
+            string = string.substring(0,string.lenght-1)
+            inputBox.value = string;
+        }
+
+        else if(b.target.id == 'plusminus'){
+            string = string(-eval(string))
+            inputBox.value = string;
+        }
+
+        else{
+            string += b.target.innerText;
+            inputBox.value = string;
+        }
+        
+    })
+})
